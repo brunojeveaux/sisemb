099e0d47947e1d019331b85ec23f11501fe36b93
brunojeveaux <brunojeveaux_13@hotmail.com>
2018-05-25 10:38:03 -0300

    Update blelev.asm

--- blelev.asm
+++ blelev.asm
@@ -3,17 +3,73 @@
 ; ----------------------------
 segment code
 ..start:
-    		mov 		ax,data
-    		mov 		ds,ax
-    		mov 		ax,stack
-    		mov 		ss,ax
-    		mov 		sp,stacktop
-        mov byte[motoracao],1
-        call move_motor
+    	mov 		ax,data
+    	mov 		ds,ax
+    	mov 		ax,stack
+    	mov 		ss,ax
+    	mov 		sp,stacktop
+	
+	ler_sensor:
+	mov 	dx,319h
+	in		dx,al
+	mov 	byte[entrada],al
+	xor 	ah,ah
+	mov		al,byte[motoreleds]			
+	shr		ax,6
+	test	byte[leitura+di],01000000b	; leitura+di para testes ; entrada para func normal
+	jz 		buraco
+	jmp 	obstruido
+	buraco:			; se mudar de 1 pra zero e estava subindo, incrementa o andar. Se não, continua o que estava fazendo.
+	mov		byte[sensor_new],0
+	test 	byte[sensor_old],1
+	jz 		continua
+	cmp		al,0	; parado
+	je		continua
+	cmp		al,1	; subindo
+	je 		incrementa_andar
+	cmp		al,2	; descendo
+	je		continua
+	cmp		al,3	; parado
+	je		continua
+	obstruido:		; se mudar de zero pra 1 e estava descendo, decrementa o andar. Se não, continua o que estava fazendo.
+	mov		byte[sensor_new],1
+	test	byte[sensor_old],1
+	jnz	 	continua
+	cmp		al,0	; parado
+	je		continua
+	cmp		al,1	; subindo
+	je	 	continua
+	cmp 	al,2	; descendo
+	je	 	decrementa_andar
+	cmp		al,3	; parado
+	je		continua
+	incrementa_andar:
+	inc		byte[andar_atual]
+	jmp 	continua
+	decrementa_andar:
+	dec 	byte[andar_atual]
+	continua:
+	mov		al,byte[sensor_new]
+	mov 	byte[sensor_old],al
+	
+	mov		al,byte[andar_atual]
+	add		al,'0'
+	mov 	byte[andar_str],al
+	mov 	dx,andar_str
+	mov 	ah,09h
+	int 	21h
+	inc		di					; para testes
+	loop 	ler_sensor			; para testes
+	
         mov    	ah,08h ; fica esperando dar enter para finalizar o programa
-		    int     21h
-        mov     ax,4c00h ; finaliza o programa
-      	int     21h
+	int     21h
+        jmp saida
+
+saida:
+	mov 	ax,4c00h
+	int		21h
+	
+
 
 move_motor: ; em motoracao está o comando desejado para o motor (0 parado, 1 subida, 2 descida, 3 parado)
         push ax
@@ -133,11 +189,14 @@ zera_valor: ; ponteiro está em bx
 
 segment data
 
-motoreleds  db  00000000b
-motoracao   db  0
-andar_atual db  1
-andar_desejado  db  4
-
+motoreleds		db  00000000b ; bits 7 e 6 são os motores 0 0 Parado; 0 1 Sobe; 1 0 Desce; 1 1 Parado
+motoracao   		db  0
+andar_atual 		db  1
+andar_desejado 		db  4
+entrada			db		00000000b	; valores lidos da porta 319h (bit 6 é o sensor)
+sensor_old		db		0		; 0 -> buraco, 1 -> ostruido
+sensor_new		db		0			
+str_			db		'fim dados'
 ; testes
 tempo   db    0
 teste  db    '0000$'
