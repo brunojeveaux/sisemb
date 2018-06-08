dc020b0991e4e07afbe176fbd12be44dca7d9fa8
brunojeveaux <brunojeveaux_13@hotmail.com>
2018-06-08 10:15:05 -0300

    Update hellevat.asm

--- hellevat.asm
+++ hellevat.asm
@@ -46,8 +46,19 @@ modo_operacao:
 	call desenha_seta_inferior
 
 	mov ax,444
+	mov bx,404
+	mov dx,1
+	mov byte[cor],vermelho
+	call pinta_seta
+
+	mov ax,444
 	mov bx,362
 	call desenha_seta_central_interna
+
+	mov ax,444
+	mov bx,362
+	mov byte[cor],vermelho
+	call pinta_seta_dupla_interna
 
 	mov ax,444
 	mov bx,270
@@ -56,22 +67,29 @@ modo_operacao:
 	mov ax,444
 	mov bx,168
 	call desenha_seta_superior
+
+	mov ax,444
+	mov bx,168
+	mov dx,-1
+	mov byte[cor],vermelho
+	call pinta_seta
 
 	mov ax,567
 	mov bx,404
 	call desenha_seta_inferior
 
 	mov ax,567
-	mov bx,362
-	call desenha_seta_central_interna
+	mov bx,374
+	call desenha_seta_central_externa
 
 	mov ax,567
-	mov bx,270
-	call desenha_seta_central_interna
+	mov bx,282
+	call desenha_seta_central_externa
 
 	mov ax,567
 	mov bx,168
 	call desenha_seta_superior
+
 
 loop_teste:
 	cmp		byte[flag_dois],1
@@ -774,6 +792,91 @@ desenha_seta_central_externa:
 
 ;**** desenha_seta_central_externa ****
 
+;**** pinta_seta ****
+; essa função pinta o botão de chamadas. Para utilizar a função, faça:
+; mov ax,coordenada_x
+; mov bx,coordenada_y ; essas coordenadas são da pontinha da seta
+; mov dx,DIRECAO			; -1 para pintar seta superior e 1 para pintar seta inferior
+; call pinta_seta
+pinta_seta:
+        push ax
+        push bx
+        push cx
+        push dx
+        push di
+
+				add bx,dx
+				push ax
+				push bx
+				call plot_xy
+
+				mov cx,18
+				mov di,1
+				pintando_seta:
+				add bx,dx
+				sub ax,di
+				push ax ; push x1
+				push bx	; push y1
+
+				add ax,di
+				add ax,di
+				push ax ; push x2
+				push bx	; push y2
+				call line
+				sub ax,di ; volta à posição central
+				inc di
+				loop pintando_seta
+
+				mov cx,20
+				; pinta o retângulo da seta
+				pintando:
+				add bx,dx
+				sub ax,9
+				push ax ; push x1
+				push bx ; push y1
+				add ax,18
+				push ax ; push x2
+				push bx ; push y2
+				sub ax,9
+				call line
+				loop pintando
+
+				pop di
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
+
+;**** pinta_seta ****
+
+;**** pinta_seta_dupla_interna ****
+; essa função pinta o botão de chamadas interna. Para utilizar a função, faça:
+; mov ax,coordenada_x
+; mov bx,coordenada_y ; essas coordenadas são da pontinha SUPERIOR da seta
+; call pinta_seta_dupla_interna
+pinta_seta_dupla_interna:
+        push ax
+        push bx
+        push cx
+        push dx
+        push di
+
+				mov byte[cor],vermelho
+				mov dx,-1
+				call pinta_seta
+				sub bx,60
+				mov dx,1
+				call pinta_seta
+
+				pop di
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret
+
+;**** pinta_seta_dupla_interna ****
 
 ; --------------- Segmento de Dados ---------------
 segment data
