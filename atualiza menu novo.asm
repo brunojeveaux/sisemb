cc20404cb33b43ba45817fc628fb88336b3e52cc
lailasr <39624524+lailasr@users.noreply.github.com>
2018-06-28 12:29:10 -0300

    Update atualiza menu novo.asm

--- atualiza menu novo.asm
+++ atualiza menu novo.asm
@@ -4,7 +4,15 @@ coo_seta_int_4  dw		444,404,'$'
 coo_seta_int_3 	dw		444,362,'$'
 coo_seta_int_2  dw		444,270,'$'
 coo_seta_int_1 	dw		444,168,'$'
+coo_seta_ext_1	dw		567,168,'$'
+coo_seta_ext_2	dw		567,198,'$'
+coo_seta_ext_3	dw		567,282,'$'
+coo_seta_ext_4	dw		567,290,'$'
+coo_seta_ext_5	dw		567,374,'$'
+coo_seta_ext_6	dw		567,404,'$'
+
 flag_interna_old db		00000000b	; guarda as anteriores para não pintar toda hora.
+flag_externa_old db		00000000b	; guarda as anteriores para não pintar toda hora.
 
 ; ***** atualiza_menu *****
 ; essa função atualiza o menu. Para utilizar a função, faça:
@@ -97,17 +105,130 @@ atualiza_menu:
 	mov		dx,1
 	call	pinta_seta
 	pop		ax
-	testa_seta_externa:
-	mov		byte[flag_interna_old],al	
-; mov ax,coordenada_x
-; mov bx,coordenada_y ; essas coordenadas são da pontinha da seta
-; mov dx,DIRECAO
-	
-	
-	
-	
-	
-continua_atualiza:	
+	testa_seta_externa: 	
+	mov		byte[flag_interna_old],al
+	jmp 	testa_seta_externa_2:
+	continua_atualiza_curto_2:
+	jmp 	continua_atualiza
+	testa_seta_externa_2:
+	; verifica se deve pintar alguma seta externa
+	mov 	ah,byte[flag_externa_old]
+	mov 	al,byte[flag_externa]
+	cmp		ah,al
+	je		continua_atualiza_curto_2
+	; testa se deve pintar a primeira seta externa
+	mov		bh,ah
+	mov		bl,al
+	and		bh,00000001b
+	and		bl,00000001b
+	cmp 	bl,bh
+	je		testa_seta_dois_ext
+	mov 	byte[cor],azul
+	cmp		bl,0
+	jne		continua_pinta_seta_1_ext
+	mov 	byte[cor],preto
+	continua_pinta_seta_1_ext:
+	push 	ax
+	mov		ax,[coo_seta_ext_1]
+	mov		bx,[coo_seta_ext_1+2]
+	mov		dx,-1
+	call	pinta_seta
+	pop		ax
+	; testa se deve pintar a segunda seta externa
+	testa_seta_dois_ext:
+	mov		bh,ah
+	mov		bl,al
+	and		bh,00000010b
+	and		bl,00000010b
+	cmp 	bl,bh
+	je		testa_seta_tres_ext
+	mov 	byte[cor],vermelho
+	cmp		bl,0
+	jne		continua_pinta_seta_2_ext
+	mov 	byte[cor],preto
+	continua_pinta_seta_2_ext:
+	push 	ax
+	mov		ax,[coo_seta_ext_2]
+	mov		bx,[coo_seta_ext_2+2]
+	mov 	dx,1
+	call	pinta_seta
+	pop		ax
+	; testa se deve pintar a terceira seta externa
+	testa_seta_tres_ext:
+	mov		bh,ah
+	mov		bl,al
+	and		bh,00000100b
+	and		bl,00000100b
+	cmp 	bl,bh
+	je		testa_seta_quatro_ext
+	mov 	byte[cor],azul
+	cmp		bl,0
+	jne		continua_pinta_seta_3_ext
+	mov 	byte[cor],preto
+	continua_pinta_seta_3_ext:
+	push 	ax
+	mov		ax,[coo_seta_ext_3]
+	mov		bx,[coo_seta_ext_3+2]
+	mov		dx,-1
+	call	pinta_seta
+	pop		ax
+	; testa se deve pintar a quarta seta interna
+	testa_seta_quatro_ext:
+	mov		bh,ah
+	mov		bl,al
+	and		bh,00001000b
+	and		bl,00001000b
+	cmp 	bl,bh
+	je		testa_seta_cinco_ext
+	mov 	byte[cor],vermelho
+	cmp		bl,0
+	jne		continua_pinta_seta_4_ext
+	mov 	byte[cor],preto
+	continua_pinta_seta_4_ext:
+	push 	ax
+	mov		ax,[coo_seta_ext_4]
+	mov		bx,[coo_seta_ext_4+2]
+	mov		dx,1
+	call	pinta_seta
+	pop		ax
+	testa_seta_cinco_ext:
+	mov		bh,ah
+	mov		bl,al
+	and		bh,00010000b
+	and		bl,00010000b
+	cmp 	bl,bh
+	je		testa_seta_seis_ext
+	mov 	byte[cor],azul
+	cmp		bl,0
+	jne		continua_pinta_seta_5_ext
+	mov 	byte[cor],preto
+	continua_pinta_seta_5_ext:
+	push 	ax
+	mov		ax,[coo_seta_ext_5]
+	mov		bx,[coo_seta_ext_5+2]
+	mov		dx,-1
+	call	pinta_seta
+	pop		ax
+	testa_seta_seis_ext:
+	mov		bh,ah
+	mov		bl,al
+	and		bh,00100000b
+	and		bl,00100000b
+	cmp 	bl,bh
+	je		continua_atualiza
+	mov 	byte[cor],vermelho
+	cmp		bl,0
+	jne		continua_pinta_seta_6_ext
+	mov 	byte[cor],preto
+	continua_pinta_seta_6_ext:
+	push 	ax
+	mov		ax,[coo_seta_ext_6]
+	mov		bx,[coo_seta_ext_6+2]
+	mov		dx,1
+	call	pinta_seta
+	pop		ax
+continua_atualiza:
+	mov		byte[flag_externa_old],al	
     ; LIMPA ANDAR ANTERIOR
     mov     al,byte[andar_anterior]
     cmp     al,byte[andar_atual]
